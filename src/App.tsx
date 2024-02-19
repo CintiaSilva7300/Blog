d12b0ea812830b44940d064484fb1b0186e4ad1f
CintiaSilva7300 <88053580+CintiaSilva7300@users.noreply.github.com>
2024-02-19 08:28:54 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,26 @@
+import "./index.css";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { Home } from "./paginas/home/Home";
+import { Login } from "./paginas/login/Login";
+import { Navbar } from "./components/navBar/NavBar";
+import { Footer } from "./components/footer/Footer";
+
+function App() {
+  return (
+    <>
+      <BrowserRouter>
+        <Navbar />
+        <div className="min-h-[80vh]">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/home" element={<Home />} />
+            <Route path="/login" element={<Login />} />
+          </Routes>
+        </div>
+        <Footer />
+      </BrowserRouter>
+    </>
+  );
+}
+
+export default App;
---
+++ src/paginas/login/Login.tsx
@@ -0,0 +1,9 @@
+export function Login() {
+  return (
+    <>
+      <div className="h-96 mb-96">
+        <h1 className="text-xl">Longin</h1>
+      </div>
+    </>
+  );
+}
