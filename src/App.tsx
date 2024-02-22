58a2d8240f0a957d19e8c43aa8e46d31521b5639
CintiaSilva7300 <88053580+CintiaSilva7300@users.noreply.github.com>
2024-02-22 13:59:21 -0300

    ajustes realizados nos componentes adicionando o toast

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,26 @@
-
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import './App.css'
-import Home from './paginas/home/Home'
-import Login from './paginas/login/Login'
-import Footer from './componentes/footer/Footer'
-import Navbar from './componentes/navbar/Navbar'
-import Cadastro from './paginas/cadastro/Cadastro'
-import { AuthProvider } from './contexts/AuthContext'
-import ListaTemas from './componentes/temas/listaTemas/ListaTemas'
-import FormularioTema from './componentes/temas/formularioTemas/FormularioTema'
-import DeletarTema from './componentes/temas/deletarTema/DeletarTema'
-
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import "./App.css";
+import Home from "./paginas/home/Home";
+import Login from "./paginas/login/Login";
+import Footer from "./componentes/footer/Footer";
+import Navbar from "./componentes/navbar/Navbar";
+import Cadastro from "./paginas/cadastro/Cadastro";
+import { AuthProvider } from "./contexts/AuthContext";
+import ListaTemas from "./componentes/temas/listaTemas/ListaTemas";
+import FormularioTema from "./componentes/temas/formularioTemas/FormularioTema";
+import DeletarTema from "./componentes/temas/deletarTema/DeletarTema";
+import ListaPostagens from "./componentes/listaPostagens/ListaPostagens";
+import FormularioPostagem from "./componentes/formularioPostagem/FormularioPostagem";
+import DeletarPostagem from "./componentes/deletarPostagem/DeletarPostagem";
+import Perfil from "./paginas/perfil/Perfil";
 
 function App() {
   return (
     <>
-    <AuthProvider>
+      <AuthProvider>
         <BrowserRouter>
           <Navbar />
-          <div className='min-h-[80vh]'>
+          <div className="min-h-[80vh]">
             <Routes>
               <Route path="/" element={<Login />} />
               <Route path="/login" element={<Login />} />
@@ -28,12 +30,21 @@ function App() {
               <Route path="/cadastroTema" element={<FormularioTema />} />
               <Route path="/editarTema/:id" element={<FormularioTema />} />
               <Route path="/deletarTema/:id" element={<DeletarTema />} />
-
+              <Route path="/postagens" element={<ListaPostagens />} />
+              <Route
+                path="/editarPostagem/:id"
+                element={<FormularioPostagem />}
+              />
+              <Route
+                path="/deletarPostagem/:id"
+                element={<DeletarPostagem />}
+              />
+              <Route path="/perfil" element={<Perfil />} />
             </Routes>
           </div>
           <Footer />
         </BrowserRouter>
-        </AuthProvider>
+      </AuthProvider>
     </>
   );
 }
--- src/contexts/AuthContext.tsx
+++ src/contexts/AuthContext.tsx
@@ -1,59 +1,62 @@
 import { ReactNode, createContext, useState } from "react";
 import UsuarioLogin from "../models/UsuarioLogin";
 import { login } from "../services/Service";
+import { toastAlerta } from "../utils/toastAlerta";
 
 interface AuthContextProps {
-    usuario: UsuarioLogin
-    handleLogout(): void
-    handleLogin(usuario: UsuarioLogin): Promise<void>
-    isLoading: boolean
+  usuario: UsuarioLogin;
+  handleLogout(): void;
+  handleLogin(usuario: UsuarioLogin): Promise<void>;
+  isLoading: boolean;
 }
 
-interface AuthProviderProps{
-    children: ReactNode
+interface AuthProviderProps {
+  children: ReactNode;
 }
 
-export const AuthContext = createContext({} as AuthContextProps)
+export const AuthContext = createContext({} as AuthContextProps);
 
 export function AuthProvider({ children }: AuthProviderProps) {
-    const [usuario, setUsuario] = useState<UsuarioLogin>({
-        id: 0,
-        nome: "",
-        usuario: "",
-        foto: "",
-        senha: "",
-        token: ""
-    })
+  const [usuario, setUsuario] = useState<UsuarioLogin>({
+    id: 0,
+    nome: "",
+    usuario: "",
+    senha: "",
+    foto: "",
+    token: "",
+  });
 
-    const[isLoading, setIsLoading] = useState(false)
+  const [isLoading, setIsLoading] = useState(false);
 
-    async function handleLogin(userLogin: UsuarioLogin) {
-        setIsLoading(true)
-        try {
-            await login(`/usuarios/logar`, userLogin, setUsuario)
-            alert("Usuário logado com sucesso")
-            setIsLoading(false)
-
-        } catch (erro){
-            console.log("Não é possível validar usuário, verifique seu e-maile senha!")
-            setIsLoading(false)
-        }
+  async function handleLogin(userLogin: UsuarioLogin) {
+    setIsLoading(true);
+    try {
+      await login(`/usuarios/logar`, userLogin, setUsuario);
+      toastAlerta("Você precisa estar logado", "info");
+      setIsLoading(false);
+    } catch (error) {
+      console.log(error);
+      toastAlerta("Você precisa estar logado", "info");
+      setIsLoading(false);
     }
+  }
 
-    function handleLogout(){
-        setUsuario({
-            id: 0,
-            nome: "",
-            usuario: "",
-            foto: "",
-            senha: "",
-            token: ""
-        })
-    }
+  function handleLogout() {
+    setUsuario({
+      id: 0,
+      nome: "",
+      usuario: "",
+      senha: "",
+      foto: "",
+      token: "",
+    });
+  }
 
-    return(
-        <AuthContext.Provider value={{usuario, handleLogin, handleLogout, isLoading}}>
-            {children}
-        </AuthContext.Provider>
-    )
+  return (
+    <AuthContext.Provider
+      value={{ usuario, handleLogin, handleLogout, isLoading }}
+    >
+      {children}
+    </AuthContext.Provider>
+  );
 }
