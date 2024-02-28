ca598147723c415a7f745136bad9f3b3452547ce
CintiaSilva7300 <88053580+CintiaSilva7300@users.noreply.github.com>
2024-02-27 21:43:08 -0300

    teste

--- src/Services/Service.ts
+++ src/Services/Service.ts
@@ -6,51 +6,51 @@ const api = axios.create({
 
 export const cadastrarUsuario = async (
   url: string,
-  dados: Record<string, unknown>,
-  setDados: (data: unknown) => void
+  dados: Record<string, any>,
+  setDados: (data: any) => void
 ) => {
-  const resposta: AxiosResponse<unknown> = await api.post(url, dados);
+  const resposta: AxiosResponse<any> = await api.post(url, dados);
   setDados(resposta.data);
 };
 
 export const login = async (
   url: string,
-  dados: Record<string, unknown>,
-  setDados: (data: unknown) => void
+  dados: Record<string, any>,
+  setDados: (data: any) => void
 ) => {
-  const resposta: AxiosResponse<unknown> = await api.post(url, dados);
+  const resposta: AxiosResponse<any> = await api.post(url, dados);
   setDados(resposta.data);
 };
 
 export const buscar = async (
   url: string,
-  setDados: (data: unknown) => void,
-  header: Record<string, unknown>
+  setDados: (data: any) => void,
+  header: Record<string, any>
 ) => {
-  const resposta: AxiosResponse<unknown> = await api.get(url, header);
+  const resposta: AxiosResponse<any> = await api.get(url, header);
   setDados(resposta.data);
 };
 
 export const cadastrar = async (
   url: string,
-  dados: Record<string, unknown>,
-  setDados: (data: unknown) => void,
-  header: Record<string, unknown>
+  dados: Record<string, any>,
+  setDados: (data: any) => void,
+  header: Record<string, any>
 ) => {
-  const resposta: AxiosResponse<unknown> = await api.post(url, dados, header);
+  const resposta: AxiosResponse<any> = await api.post(url, dados, header);
   setDados(resposta.data);
 };
 
 export const atualizar = async (
   url: string,
-  dados: Record<string, unknown>,
-  setDados: (data: unknown) => void,
-  header: Record<string, unknown>
+  dados: Record<string, any>,
+  setDados: (data: any) => void,
+  header: Record<string, any>
 ) => {
-  const resposta: AxiosResponse<unknown> = await api.put(url, dados, header);
+  const resposta: AxiosResponse<any> = await api.put(url, dados, header);
   setDados(resposta.data);
 };
 
-export const deletar = async (url: string, header: Record<string, unknown>) => {
+export const deletar = async (url: string, header: Record<string, any>) => {
   await api.delete(url, header);
 };
